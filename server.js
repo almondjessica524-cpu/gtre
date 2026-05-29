24b95f696b9b60b00b4bd2dd42da43f8735b0cbb
almondjessica524-cpu <almondjessica524@gmail.com>
2026-05-29 17:39:36 +0100

    Update server.js

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const allowedOrigins = [
   'https://tramd.netlify.app',
   'https://belltrek.netlify.app',
   'https://sunny-pastelito-8843b7.netlify.app',
-  'https://viralbell.netlify.app',
+  'https://tramd.netlify.app',
   'https://jhvnb.vercel.app',
    // add all your trusted frontends here
 ];
